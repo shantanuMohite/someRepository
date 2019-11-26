b5dedb3bbdb9b326cb875a4947369082bec28ae7
shantanuMohite <shantanu@tintin.com>
2019-11-26 15:55:50 +0530

    nothing

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ var config = require("config");
 var port =   parseInt(config.get("port"));
 var app =  express();
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/emps", empsRouter);
 app.use("/admin", adminRouter);
