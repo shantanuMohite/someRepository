2fb7011de835bc7f913f9340659171c9a13089c0
shantanuMohite <shantanu@tintin.com>
2019-11-26 15:40:57 +0530

    ytrar

--- new.js
+++ new.js
@@ -7,18 +7,13 @@ adminRouter.get("/", (request, response)=>
     response.send("This is Admin Page");
 });
 
-adminRouter.get("/:ANo", (request, response)=>
-{
-    response.send("You searched for Admin No " + request.params.ANo);
-});
 
-adminRouter.get("/:ANo", (request, response)=>
-{
-    response.send("You searched for Admin No " + request.params.ANo);
-});
 adminRouter.post("/", (request, response)=>
 {
     response.send("You inserted ");
 });
+
+
+
 
 module.exports = adminRouter;
